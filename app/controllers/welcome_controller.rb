00b371c62f348ee8501a6901b8bb69e81258a176
Rick Little <rick.little@simplarity.com>
2015-08-17 20:14:51 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,23 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def early
+  end
+
+  def education
+  end
+
+  def career
+  end
+
+  def certifications
+  end
+
+  def family
+  end
+
+  def contact
+  end
+
+end
